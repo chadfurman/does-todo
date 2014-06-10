6c001abb1847c62d1228037727225dfb0df3f264
Chad Furman <chad@fuzzproductions.com>
2014-06-09 20:50:17 -0400

    added famono demo code

--- fuzz-todo.js
+++ fuzz-todo.js
@@ -1,19 +1,37 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to fuzz-todo.";
-  };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  // Rig some famo.us deps
+  famousPolyfills;
+  famous.core.famous;
+
+  // Make sure dom got a body...
+  Meteor.startup(function() {
+    var mainContext = famous.core.Engine.createContext();
+    var renderController = new famous.views.RenderController();
+    var surfaces = [];
+    var counter = 0;
+
+    for (var i = 0; i < 10; i++) {
+        surfaces.push(new famous.core.Surface({
+             content: "Surface: " + (i + 1),
+             size: [200, 200],
+             properties: {
+                 backgroundColor: "hsl(" + (i * 360 / 10) + ", 100%, 50%)",
+                 lineHeight: "200px",
+                 textAlign: 'center'
+             }
+        }));
     }
-  });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+    renderController.show(surfaces[0]);
+
+    famous.core.Engine.on("click", function() {
+        var next = (counter++ + 1) % surfaces.length;
+        this.show(surfaces[next]);
+    }.bind(renderController));
+
+    mainContext.add(new famous.core.Modifier({origin: [.5, .5]})).add(renderController);
+
   });
+
 }
