3d99601d644a375cf1716fddd69a123e34c9cb54
Chad Furman <chad@fuzzproductions.com>
2014-06-09 20:18:50 -0400

    fuzz todo alpha template

--- private/js/main.js
+++ private/js/main.js
@@ -3,26 +3,42 @@ define(function(require, exports, module) {
 	var Utility = require('famous/utilities/Utility');
 	var InputSurface = require("famous/surfaces/InputSurface");
 	var SequentialLayout = require("famous/views/SequentialLayout");
-	var test = require("test");
-
+	var EventHandler = require('famous/core/EventHandler');
+	var Transform = require('famous/core/Transform');
 
 	var mainContext = Engine.createContext();
 	var todoInputLayout = new SequentialLayout({direction:Utility.Direction.X});
+	var eventHandler = new EventHandler();
 	var todoInputSequence = [];
 
-	todoInputSequence.push(new InputSurface({
+	var textbox = new InputSurface({
 		size: [200,50],
 		name:'new-todo',
 		placeholder:'e.x. "Add Todo Items"',
 		type: 'text',
 		class: 'new-todo'
-	}));
-	todoInputSequence.push(new InputSurface({
+	});
+
+	var button = new InputSurface({
 		size: [75,50],
 		type: 'button',
 		value: 'Go',
 		class: 'btn todo-go-btn'
-	}));
+	});
+
+	button.on('click', function() {
+		eventHandler.emit('save-todo');
+	});
+
+	eventHandler.on('save-todo', function() {
+		textbox.setTransform({
+			transform: Transform.translate(45,45)
+		})
+//		console.log('tom!');
+	})
+
+	todoInputSequence.push(textbox);
+	todoInputSequence.push(button);
 
 	todoInputLayout.sequenceFrom(todoInputSequence);
 	mainContext.add(todoInputLayout);
